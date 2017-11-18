7141d071484c415a2ac12f52086db108d5215ea7
Viko@raspberry <victor@stationf.co>
2017-11-18 21:46:45 +0100

    update ip

--- app/src/config/stargazer.js
+++ app/src/config/stargazer.js
@@ -3,8 +3,8 @@ exports.logoName	= "StarGazer";
 const ip = "localhost";
 
 if (process.env.NODE_ENV === "development") {
-	exports.apiUrl		= "http://79.137.36.192:8080";
-	exports.urlApp		= "http://79.137.36.192:3000";
+	exports.apiUrl		= "http://localhost:8080";
+	exports.urlApp		= "http://localhost:3000";
 } else {
 	exports.apiUrl		= "http://"+ip+":8080"
 	exports.urlApp		= "http://"+ip+":5000"
